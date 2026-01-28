3f844eef7410a526ccd370b078fe12f070a6fa7c
Maasha <maasha445@gmail.com>
2026-01-27 21:36:42 -0500

    Update producer.py

--- producer.py
+++ producer.py
@@ -1,35 +1,3 @@
-# from kafka import KafkaProducer
-# import json
-# import time
-# import requests
-# from datetime import datetime
-
-# producer = KafkaProducer(
-#     bootstrap_servers="localhost:9092",
-#     value_serializer=lambda v: json.dumps(v).encode("utf-8")
-# )
-
-# COINS = ["bitcoin", "ethereum", "solana"]
-
-# def get_price(coin):
-#     url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin}&vs_currencies=usd"
-#     r = requests.get(url).json()
-#     print(r)
-#     return r[coin]["usd"]
-
-# while True:
-#     for coin in COINS:
-#         price = get_price(coin)
-#         data = {
-#             "coin": coin,
-#             "price": price,
-#             "event_time": datetime.utcnow().isoformat()
-#         }
-#         producer.send("crypto_prices", value=data)
-#         print("Sent:", data)
-#     time.sleep(5)
-
-
 from kafka import KafkaProducer
 import json
 import time
